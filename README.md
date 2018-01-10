ed07acd5c766c6781161197844e7f66d19821845
ChunhuanLin <lch_xdu@163.com>
2018-01-10 11:14:18 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## PyTorch Implementation of  Deformable Convolution  
+This repository implements the defromable convolution architecture proposed in this paper.  
+[Deformable Convolutional Networks](https://arxiv.org/abs/1703.06211)  
+
+### Usage
+* The defromable convolution module, i.e., *DeformConv2D*, is defined in `deform_conv.py`.  
+* A simple demo is shown in 'demo.py', it's easy to interpolate the *DeformConv2D* module into your own networks.  
+
+### Statement
+* Previous [PyTorch](https://github.com/oeway/pytorch-deform-conv)/[TensorFlow](https://github.com/felixlaumon/deform-conv) implementation are different from the original paper as discussed in this [issue](https://github.com/felixlaumon/deform-conv/issues/4), which motivates me to do a new implementation in this repo.  
+* In my opinion, the *DeformConv2D* module is better added to top of higher-level features for the sake of better learning the offsets. More experiments are needed to validate this conjecture.
+
---
+++ deform_conv.py
@@ -0,0 +1,116 @@
+import os
+from math import sqrt
+from torch.autograd import Variable
+import torch
+from torch import nn
+import numpy as np
+from pprint import pprint
+
+
+class DeformConv2D(nn.Module):
+    def __init__(self, inc, outc, kernel_size=3, padding=1):
+        super(DeformConv2D, self).__init__()
+        self.kernel_size = kernel_size
+        self.N = kernel_size**2
+        self.padding = padding
+        # Note: As illustrated in the paper, conv_offset's weights should be initialed with 0.
+        self.conv_offset = nn.Conv2d(inc, 2*self.N, kernel_size=kernel_size, padding=padding)
+        self.conv_kernel = nn.Conv2d(inc, outc, kernel_size=kernel_size, stride=kernel_size)
+
+    def forward(self, x):
+        # (b, 2N, h, w)
+        offset = self.conv_offset(x)
+        dtype = offset.data.type()
+        b, c, h, w = x.size()
+        N = self.N
+        zero = Variable(torch.FloatTensor([0]).type_as(offset.data), requires_grad=True)
+
+        # (1, 2N, 1, 1)
+        p_n = self._get_p_n(dtype)
+
+        # (1, 2N, h, w)
+        p_0 = self._get_p_0(x.size(), dtype)
+
+        p = (p_0 + p_n + offset).unsqueeze(dim=-1).unsqueeze(dim=-1)
+        # (b, 2N, h, w, 1, 1)
+        p = p.expand(-1, -1, -1, -1, 1, 1)
+
+        # (h, w)
+        q = self._get_q(x.size(), dtype)
+
+        # (b, N, h, w, h, w)
+        G = torch.max((1-torch.abs(p[:, :N, :, :, :, :] - q[0, :, :])), zero)\
+            * torch.max((1-torch.abs(p[:, N:, :, :, :, :] - q[1, :, :])), zero)
+        # (b, N*h*w, h*w)
+        G = G.contiguous().view(b, N*h*w, -1)
+        # (b, h*w, c)
+        x = x.permute(0, 2, 3, 1).contiguous().view(b, -1, c)
+        # (b, c, h, w, N)
+        x_offset = torch.bmm(G, x).contiguous().view(b, N, h, w, c).permute(0, 4, 2, 3, 1)
+        # (b, c, h*kernel_size, w*kernel_size)
+        x_offset = self._reshape_x_offset(x_offset)
+        out = self.conv_kernel(x_offset)
+
+        return out
+
+    def _get_p_n(self, dtype):
+        p_n_x, p_n_y = np.meshgrid(range(-(self.kernel_size-1)//2, (self.kernel_size-1)//2+1),
+                          range(-(self.kernel_size-1)//2, (self.kernel_size-1)//2+1), indexing='ij')
+        # (2N, 1), order[x1, x2, ..., y1, y2, ...]
+        p_n = np.concatenate((p_n_x.flatten(), p_n_y.flatten()))
+        p_n = np.reshape(p_n, (1, 2*self.N, 1, 1))  # .repeat(b, axis=0).repeat(h, axis=2).repeat(w, axis=3)
+        p_n = Variable(torch.from_numpy(p_n).type(dtype), requires_grad=False)
+
+        return p_n
+
+    def _get_p_0(self, x_size, dtype):
+        b, c, h, w = x_size
+        p_0_x, p_0_y = np.meshgrid(range(0, h), range(0, w), indexing='ij')
+        p_0_x = p_0_x.flatten().reshape(1, 1, h, w).repeat(self.N, axis=1)
+        p_0_y = p_0_y.flatten().reshape(1, 1, h, w).repeat(self.N, axis=1)
+        p_0 = np.concatenate((p_0_x, p_0_y), axis=1)
+        p_0 = Variable(torch.from_numpy(p_0).type(dtype), requires_grad=False)
+
+        return p_0
+
+    def _get_q(self, x_size, dtype):
+        b, c, h, w = x_size
+        q_x, q_y = np.meshgrid(range(0, h), range(0, w), indexing='ij')
+        q_x = q_x.flatten().reshape(1, h, w)
+        q_y = q_y.flatten().reshape(1, h, w)
+        q = np.concatenate((q_x, q_y))
+        q = Variable(torch.from_numpy(q).type(dtype), requires_grad=False)
+
+        return q
+
+    def _reshape_x_offset(self, x_offset):
+        b, c, h, w, N = x_offset.size()
+        ks = int(sqrt(N))
+        x_offset = torch.cat([x_offset[..., s:s+ks].contiguous().view(b, c, h, w*ks) for s in range(0, N, ks)], dim=-1)
+        x_offset = x_offset.contiguous().view(b, c, h*ks, w*ks)
+
+        return x_offset
+
+# N, inC, inH, inW = 2, 6, 30, 40
+# kH, kW = 3, 3
+#
+# # set devices
+# # ---------------------------------------
+# use_gpu = torch.cuda.is_available()
+# gpu_device = 0
+# if use_gpu:
+#     os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu_device)
+#     print("Using gpu{}".format(os.getenv("CUDA_VISIBLE_DEVICES")))
+# # ---------------------------------------
+#
+# inputs = Variable(torch.randn(N, inC, inH, inW).cuda())
+# model = DeformConv2D(inC, 1).cuda()
+#
+# learning_rate = 1e-4
+# optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+#
+# for i in range(5):
+#     optimizer.zero_grad()
+#     output = model(inputs)
+#     output.backward(output.data)
+#     optimizer.step()
